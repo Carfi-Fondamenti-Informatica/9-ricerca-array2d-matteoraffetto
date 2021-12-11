fb750ee6c11f415d550912ad98400304e394fb07
matteoraffetto <91629988+matteoraffetto@users.noreply.github.com>
2021-12-11 13:03:58 +0100

    Update lib.h

--- lib.h
+++ lib.h
@@ -1,1 +1,9 @@
+//
+// Created by matte on 11/12/2021.
+//
+
+#ifndef ESERCITAZIONE9_0_LIBB_H
+#define ESERCITAZIONE9_0_LIBB_H
+bool ricerca(char lista[10][20],char nome[1][20],int &pos);
+#endif //ESERCITAZIONE9_0_LIBB_H
 
