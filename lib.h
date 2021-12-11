cfed61336931f619f2e8c9cd1f2985ac27c61f97
matteoraffetto <91629988+matteoraffetto@users.noreply.github.com>
2021-12-11 13:06:00 +0100

    Update lib.h

--- lib.h
+++ lib.h
@@ -2,8 +2,8 @@
 // Created by matte on 11/12/2021.
 //
 
-#ifndef ESERCITAZIONE9_0_LIBB_H
-#define ESERCITAZIONE9_0_LIBB_H
+#ifndef ESERCITAZIONE9_0_LIB_H
+#define ESERCITAZIONE9_0_LIB_H
 bool ricerca(char lista[10][20],char nome[1][20],int &pos);
-#endif //ESERCITAZIONE9_0_LIBB_H
+#endif //ESERCITAZIONE9_0_LIB_H
 
