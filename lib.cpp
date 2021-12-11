46026551be84f27d2b9729418274fcebb3e977b0
matteoraffetto <91629988+matteoraffetto@users.noreply.github.com>
2021-12-11 13:04:26 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,15 @@
+//
+// Created by matte on 11/12/2021.
+//
 
+#include "libb.h"
+#include <cstring>
+bool ricerca(char lista[10][20],char nome[1][20],int &pos){
+    for(int i=0;i<10;i++){
+        if(strcmp(lista[i],nome[0])==0){
+            pos=1;
+            return true;
+        }
+    }
+    return false;
+}
